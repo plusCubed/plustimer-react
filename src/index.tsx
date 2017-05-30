f2ff3c2665575c79f66ce4ffa38966ff75133db0
plusCubed <plusCubed@users.noreply.github.com>
2017-05-29 18:33:15 -0700

    Use Redux DevTools package

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,15 @@ import './index.css';
 
 import rootEpic from './epics/index';
 import rootReducer from './reducers/index';
-import {applyMiddleware, compose, createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {createEpicMiddleware} from 'redux-observable';
+import {composeWithDevTools} from 'redux-devtools-extension/logOnlyInProduction';
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const epicMiddleware = createEpicMiddleware(rootEpic);
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(epicMiddleware))
+    composeWithDevTools(applyMiddleware(epicMiddleware))
 );
 
 ReactDOM.render(
