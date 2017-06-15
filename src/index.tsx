ec03173e69ab3fd02720f2499e74a4d416dbea6b
plusCubed <plusCubed@users.noreply.github.com>
2017-06-14 23:47:43 -0700

    Integrate with React DevTools

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,8 @@ import {createEpicMiddleware} from 'redux-observable';
 import {composeWithDevTools} from 'redux-devtools-extension/logOnlyInProduction';
 import {ScrambleService} from './services/scramble-service';
 
+import 'preact/devtools';
+
 const solvesService = new SolvesService();
 const scrambleService = new ScrambleService();
 
