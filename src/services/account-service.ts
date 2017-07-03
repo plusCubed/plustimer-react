960ec8ff8993b908185cba6da83047efd13d2607
plusCubed <plusCubed@users.noreply.github.com>
2017-07-03 01:04:46 -0700

    Add working login with local and production backends

--- src/services/account-service.ts
+++ src/services/account-service.ts
@@ -1,18 +1,44 @@
 import superlogin from 'superlogin-client';
 import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/observable/forkJoin';
 
 export class AccountService {
 
+    private static ENDPOINT = process.env.NODE_ENV === 'production' ?
+        'https://timer-sync.pluscubed.com' :
+        'https://localhost:4000';
+
+    private authSession$: Subject<any>;
+
     constructor() {
         const config = {
+            socialUrl: AccountService.ENDPOINT + '/auth',
             baseUrl: '/auth',
             storage: 'local',
             providers: ['wca']
         };
         superlogin.configure(config);
+
+        this.authSession$ = new Subject();
+
+        const listener = (e: MessageEvent) => {
+            if (e.origin === AccountService.ENDPOINT) {
+                this.authSession$.next(e.data);
+            }
+        };
+        window.addEventListener('message', listener);
     }
 
     login(): Observable<any> {
-        return Observable.fromPromise(superlogin.socialAuth('wca'));
+        // Workaround superlogin-client expecting direct window.opener call
+        const login = Observable.fromPromise(superlogin.socialAuth('wca'))
+            .catch(err => {
+                return Observable.of(err);
+            });
+        const response = this.authSession$.take(1);
+        return Observable.forkJoin(login, response)
+            .map(result => result[1]);
     }
 }
