d0619ba122376a0a04dae839233478eacf9a1d14
plusCubed <plusCubed@users.noreply.github.com>
2017-07-30 16:32:04 -0700

    Use mdc for the entire toolbar

--- src/components/TimerAppBar.tsx
+++ src/components/TimerAppBar.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 
 import './TimerAppBar.css';
 
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Button from 'material-ui/Button';
-
+import Button from 'preact-material-components/Button';
+import Toolbar from 'preact-material-components/Toolbar';
 import Select from 'preact-material-components/Select';
+import 'preact-material-components/Button/style.css';
+import 'preact-material-components/Toolbar/style.css';
 import 'preact-material-components/List/style.css';
 import 'preact-material-components/Menu/style.css';
 import 'preact-material-components/Select/style.css';
@@ -44,46 +44,48 @@ const TimerAppBar = ({
   onCategorySelected
 }: Props) => {
   return (
-    <AppBar>
-      <Toolbar className="mdc-theme--dark">
-        <Select
-          className="puzzle-select"
-          onChange={onPuzzleSelected}
-          selectedIndex={selectedPuzzle}
-        >
-          {puzzles.map((option, index) =>
-            <Select.Item key={option}>
-              {option}
-            </Select.Item>
-          )}
-        </Select>
+    <Toolbar className="mdc-theme--dark app-bar" fixed={true}>
+      <Toolbar.Row>
+        <Toolbar.Section align-start={true}>
+          <Select
+            className="puzzle-select"
+            onChange={onPuzzleSelected}
+            selectedIndex={selectedPuzzle}
+          >
+            {puzzles.map((option, index) =>
+              <Select.Item key={option}>
+                {option}
+              </Select.Item>
+            )}
+          </Select>
 
-        <Select
-          className="category-select"
-          onChange={onCategorySelected}
-          selectedIndex={selectedCategory}
-        >
-          {categories.map((option, index) =>
-            <Select.Item key={option}>
-              {option}
-            </Select.Item>
-          )}
-        </Select>
+          <Select
+            className="category-select"
+            onChange={onCategorySelected}
+            selectedIndex={selectedCategory}
+          >
+            {categories.map((option, index) =>
+              <Select.Item key={option}>
+                {option}
+              </Select.Item>
+            )}
+          </Select>
+        </Toolbar.Section>
 
-        <div className="app-bar-spacer" />
-
-        {loggedIn
-          ? <img
-              className="app-bar-avatar"
-              alt={avatarAltName}
-              src={avatarImg}
-              onClick={onAvatarClick}
-            />
-          : <Button color="contrast" onClick={onLoginClick}>
-              Login
-            </Button>}
-      </Toolbar>
-    </AppBar>
+        <Toolbar.Section align-end={true}>
+          {loggedIn
+            ? <img
+                className="app-bar-avatar"
+                alt={avatarAltName}
+                src={avatarImg}
+                onClick={onAvatarClick}
+              />
+            : <Button ripple={true} onClick={onLoginClick}>
+                Login
+              </Button>}
+        </Toolbar.Section>
+      </Toolbar.Row>
+    </Toolbar>
   );
 };
 
