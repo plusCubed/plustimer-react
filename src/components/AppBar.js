a7796d676a65c3b72e93ac6e438d863ca3380564
plusCubed <pluscubed@users.noreply.github.com>
2017-12-25 00:45:58 -0800

    Experiment with tabbed layout on mobile

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,7 +2,10 @@
 
 import * as React from 'react';
 
+import Media from 'react-media';
+
 import style from './AppBar.css';
+import tabsStyle from './Tabs.css';
 
 type Props = {
   loggedIn: boolean,
@@ -11,22 +14,51 @@ type Props = {
   onAvatarClick: () => void
 };
 
+const Tab = ({ active, children }) => {
+  return (
+    <div className={`${tabsStyle.tab} ${active ? 'active' : ''}`}>
+      {children}
+    </div>
+  );
+};
+
+const Tabs = ({
+  options,
+  onTabSelected
+}: {
+  options: string[],
+  onTabSelected: selected => void
+}) => {
+  return (
+    <div className={tabsStyle.tabs}>
+      {options.map(option => <Tab>{option}</Tab>)}
+    </div>
+  );
+};
+
 const AppBar = (props: Props) => {
   return (
-    <header className={style.toolbar}>
-      <div className={style.toolbarText}>plusTimer</div>
-      {props.loggedIn ? (
-        <img
-          className={style.toolbarAvatar}
-          alt="User Avatar"
-          src={props.avatarImg}
-          onClick={props.onAvatarClick}
-        />
-      ) : (
-        <button className={style.toolbarButton} onClick={props.onLoginClick}>
-          Login
-        </button>
-      )}
+    <header className={style.appbar}>
+      <div className={style.toolbar}>
+        <div className={style.toolbarText}>plusTimer</div>
+        {props.loggedIn ? (
+          <img
+            className={style.toolbarAvatar}
+            alt="User Avatar"
+            src={props.avatarImg}
+            onClick={props.onAvatarClick}
+          />
+        ) : (
+          <button className={style.toolbarButton} onClick={props.onLoginClick}>
+            Login
+          </button>
+        )}
+      </div>
+
+      <Media
+        query="(max-width: 599px)"
+        render={() => <Tabs options={['Timer', 'Solves']} />}
+      />
     </header>
   );
 };
