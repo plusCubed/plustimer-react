ea023999f16a2628bda192081d34237581d8ddab
plusCubed <pluscubed@users.noreply.github.com>
2018-01-07 13:14:21 -0800

    Fix prerendering

--- src/utils/scrambleService.js
+++ src/utils/scrambleService.js
@@ -8,39 +8,47 @@ class ScrambleService {
   nextCommandId = 0;
 
   constructor() {
-    this.worker = new ScrambleWorker();
+    if (typeof window !== 'undefined') {
+      this.worker = new ScrambleWorker();
 
-    this.requestedScrambles = mitt();
+      this.requestedScrambles = mitt();
 
-    this.worker.addEventListener(
-      'message',
-      e => {
-        this.requestedScrambles.emit(e.data.commandId, e);
-      },
-      false
-    );
+      this.worker.addEventListener(
+        'message',
+        e => {
+          this.requestedScrambles.emit(e.data.commandId, e);
+        },
+        false
+      );
+    }
   }
 
   getScramble(scrambler: string): Promise<string> {
-    const commandId = this.nextCommandId;
-    this.nextCommandId++;
+    if (typeof window !== 'undefined') {
+      const commandId = this.nextCommandId;
+      this.nextCommandId++;
 
-    const scramblePromise = new Promise(resolve => {
-      const listener = e => {
-        this.requestedScrambles.off(commandId, listener);
-        const scramble = e.data.scramble.scrambleString.replace(/<br>/g, '\n');
-        resolve(scramble);
-      };
-      this.requestedScrambles.on(commandId, listener);
-    });
+      const scramblePromise = new Promise(resolve => {
+        const listener = e => {
+          this.requestedScrambles.off(commandId, listener);
+          const scramble = e.data.scramble.scrambleString.replace(
+            /<br>/g,
+            '\n'
+          );
+          resolve(scramble);
+        };
+        this.requestedScrambles.on(commandId, listener);
+      });
 
-    this.worker.postMessage({
-      command: 'getRandomScramble',
-      commandId: commandId,
-      eventName: scrambler
-    });
-
-    return scramblePromise;
+      this.worker.postMessage({
+        command: 'getRandomScramble',
+        commandId: commandId,
+        eventName: scrambler
+      });
+      return scramblePromise;
+    } else {
+      return Promise.resolve();
+    }
   }
 }
 
